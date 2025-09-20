c81f1e8671f0ae3a41f118e724f90ffd1c0b370b
OctavioCaminotti <octaviocaminotti123@gmail.com>
2025-09-20 20:51:50 -0300

    main paralelo

--- js/tema.js
+++ js/tema.js
@@ -3,14 +3,13 @@ let label_toggle = document.getElementById(`label_toggle`);
 
 toggle.addEventListener(`change`, (event)=>{
     let checked = event.target.checked;
-    document.body.classList.add(`dark`);
     if(checked){
         document.body.classList.add('dark');
         document.body.classList.remove('light');
-        label_toggle.innerHTML = `<i class="fa-regular fa-sun"></i>`;
+        label_toggle.innerHTML = `<i class="fa-regular fa-sun icon-sun"></i>`;
     }else{
         document.body.classList.add('light');
         document.body.classList.remove('dark');
-        label_toggle.innerHTML = `<i class="fa-regular fa-moon"></i>`;
+        label_toggle.innerHTML = `<i class="fa-regular fa-moon icon-moon"></i>`;
     }
 });
